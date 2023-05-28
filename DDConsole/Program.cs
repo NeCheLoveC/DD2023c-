442de6b804b2ef26e34d6da15843021196996d62
Anton Protsenko <protseng@mail.ru>
2023-05-29 02:05:07 +0400

    Все изменения

--- DDConsole/Program.cs
+++ DDConsole/Program.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using TextParser;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace DDConsole
 {
@@ -22,9 +23,24 @@ namespace DDConsole
             if (System.IO.File.Exists(fullPathName))
             {
                 String text = File.ReadAllText(fullPathName);
-                Type type = typeof(WordCalculator); 
-                Dictionary<String, int> dictionary = (Dictionary<String, int>)type.GetMethod("Parse", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, parameters: new Object[] {(object)text}); ;
-                writeDictionaryIntoFile(dictionary);
+                Type type1 = typeof(WordCalculator);
+                MethodInfo methodInfo1 = type1.GetMethod("Parse", BindingFlags.NonPublic | BindingFlags.Static);
+                Stopwatch stopwatch1 = new Stopwatch();
+                stopwatch1.Start();
+                Dictionary<String, int> dictionary = (Dictionary<String, int>)methodInfo1.Invoke(null, parameters: new Object[] {text});
+                stopwatch1.Stop();
+                Console.WriteLine("Время выполнение статического приватного метода - " + stopwatch1.ElapsedMilliseconds);
+
+
+                Stopwatch stopwatch2 = new Stopwatch();
+                WordCalculatorThread wordCalculatorThread = new WordCalculatorThread();
+                stopwatch2.Start();
+                Dictionary<String, int> dictionary2 = wordCalculatorThread.Parse(text);
+                stopwatch2.Stop();
+                Console.WriteLine("Время выполнение нестатического публичного метода - " + stopwatch2.ElapsedMilliseconds);
+
+                writeDictionaryIntoFile(dictionary2);
+                Console.ReadKey();
             }
             else
             {
--- TextParser/WordCalculator.cs
+++ TextParser/WordCalculator.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace TextParser
 {
@@ -14,7 +15,6 @@ namespace TextParser
             MatchCollection matches = regex.Matches(text);
             foreach (Match a in matches)
             {
-                Console.WriteLine(a.Value);
                 addWordIntoDictionary(a.Value, dictionary);
             }
             return dictionary = dictionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
@@ -34,5 +34,125 @@ namespace TextParser
                 dictionary.Add(str, newWordFrequency);
             }
         }
+    }
+
+
+    delegate void AddingWordIntoDictionary(String str);
+
+
+
+    public class WordCalculatorThread
+    {
+        private Dictionary<string, int> dictionary;
+        private ICollection<BagOfWords> bags;
+
+
+        public Dictionary<string, int> Parse(String text)
+        {
+            dictionary = new Dictionary<string, int>();
+            Regex regex = new Regex(@"\b\w+\b");
+            MatchCollection matches = regex.Matches(text);
+            int countOfWords = matches.Count;
+            AddingWordIntoDictionary dest = new AddingWordIntoDictionary(addWordIntoDictionary);
+            bags = BagOfWords.SplitCollectionIntoBags(matches, dest);
+
+            foreach (BagOfWords b in bags)
+            {
+                Thread thread = new Thread(b.addWordsFromBagToDictionary);
+            }
+
+            /*
+            foreach (Match a in matches)
+            {
+                Console.WriteLine(a.Value);
+                addWordIntoDictionary(a.Value, dictionary);
+            }
+            */
+            return dictionary = dictionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        private void addWordIntoDictionary(String str)
+        {
+            if (!dictionary.ContainsKey(str))
+            {
+                dictionary.Add(str, 1);
+            }
+            else
+            {
+                int currentWordFrequency = dictionary.GetValueOrDefault(str);
+                int newWordFrequency = currentWordFrequency + 1;
+                dictionary.Remove(str);
+                dictionary.Add(str, newWordFrequency);
+            }
+        }
+    }
+
+    class BagOfWords
+    {
+        private readonly object lockObj;
+        private int startId;//Включая
+        private int endId; //Не включая
+        private MatchCollection collectionOfWords;
+        //Обезопасит 
+        private AddingWordIntoDictionary addWord;
+
+        private BagOfWords(int StartId, int EndId, MatchCollection CollectionOfWords, AddingWordIntoDictionary Dest, object LockObj)
+        {
+            (startId, endId, collectionOfWords, addWord, lockObj) = (StartId, EndId, CollectionOfWords, Dest, LockObj);
+        }
+
+        public static ICollection<BagOfWords> SplitCollectionIntoBags(MatchCollection matchCollection, AddingWordIntoDictionary dest)
+        {
+            ICollection<BagOfWords> bagOfWords = new LinkedList<BagOfWords>();
+            int countOfWords = matchCollection.Count;
+            object objLock = new object();
+            //1 BagOfWords
+            if (matchCollection.Count < 500)
+            {
+                BagOfWords bag = new BagOfWords(0, countOfWords, matchCollection, dest, objLock);
+                bagOfWords.Add(bag);
+            }
+            //2 BagOfWords
+            else if (matchCollection.Count < 1000)
+            {
+                int halfOfSize = countOfWords / 2;
+                BagOfWords bag = new BagOfWords(0, halfOfSize, matchCollection, dest, objLock);
+                bagOfWords.Add(bag);
+
+                bag = new BagOfWords(bag.endId, countOfWords, matchCollection, dest, objLock);
+                bagOfWords.Add(bag);
+            }
+            //4 BagOfWords
+            else
+            {
+                int quarterOfSize = countOfWords / 4;
+                BagOfWords bag = new BagOfWords(0, quarterOfSize, matchCollection, dest, objLock);
+                bagOfWords.Add(bag);
+
+                bag = new BagOfWords(bag.endId, bag.endId + quarterOfSize, matchCollection, dest, objLock);
+                bagOfWords.Add(bag);
+
+                bag = new BagOfWords(bag.endId, bag.endId + quarterOfSize, matchCollection, dest, objLock);
+                bagOfWords.Add(bag);
+
+                bag = new BagOfWords(bag.endId, countOfWords, matchCollection, dest, objLock);
+                bagOfWords.Add(bag);
+            }
+            return bagOfWords;
+        }
+
+        public void addWordsFromBagToDictionary()
+        {
+            for (int i = startId; i < endId; i++)
+            {
+                //Вот этот код должен быть безопасным
+                lock (lockObj)
+                {
+                    addWord(collectionOfWords.ElementAt(i).Value);
+                }
+
+            }
+        }
+
     }
 }
